d3d6f228b31ef88a213e8603143d2eb996288265
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-03 08:18:13 +0500

    Explaining the caesar_cypher's program using comments to enhance readability of the code

--- program.py
+++ program.py
@@ -1,21 +1,29 @@
+# creating a function to encrypt given text by shifting each alphabet by +3
 def caesar_encrypt(text):
-    shift = 3
+    shift = 3 # setting a default shift 
     encrypted_text = ""
+    
+    # looping over the whole given string
     for char in text:
-        if char.islower():
+        # dealing with lowercase alphabets
+        if char.islower(): 
             position = ord(char) - ord('a')
             new_position = (position + shift) % 26
             new_char = chr(new_position + ord('a'))
             encrypted_text = encrypted_text + new_char
+        # dealing with uppercase alphabets
         elif char.isupper():
             position = ord(char) - ord('A')
             new_position = (position + shift) % 26
             new_char = chr(new_position + ord('A'))
             encrypted_text = encrypted_text + new_char
+        # dealing with characters other than alphabets
         else:
-            encrypted_text += char
+            encrypted_text = encrypted_text + char
+    # returning the encrypted text
     return encrypted_text
 
-message = input("Enter text to encrypt: ")
-result = caesar_encrypt(message)
-print("Encrypted text:", result)
+# main program
+message = input("Enter text to encrypt: ") # taking input from user
+result = caesar_encrypt(message) # call to caesar encrypt function
+print("Encrypted text:", result) # displaying encrypted text 
